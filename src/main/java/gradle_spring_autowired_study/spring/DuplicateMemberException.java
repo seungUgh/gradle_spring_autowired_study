b09f1dbcaa663e4cdbac1125b602cc63273029c3
seungeun <tmddms0931@naver.com>
2020-09-23 09:40:13 +0900

    기본설정

---
+++ src/main/java/gradle_spring_autowired_study/spring/DuplicateMemberException.java
@@ -0,0 +1,8 @@
+package gradle_spring_autowired_study.spring;
+
+public class DuplicateMemberException extends RuntimeException {
+	
+	public DuplicateMemberException(String message) {
+		super(message);
+	}
+}
